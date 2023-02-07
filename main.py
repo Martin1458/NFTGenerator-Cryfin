886ae73ba9ab34937193e5a11bf62d463a24c8db
Martin1458 <martinisbored1@gmail.com>
2023-02-07 22:12:24 +0100

    V2

--- main.py
+++ main.py
@@ -1,35 +1,144 @@
-from DrawSVG.drawSVG import SVG
+from PIL import Image 
+from IPython.display import display 
+import random
+import json
+import os
 import shutil
+from partsManager import *
+from jsonToCsv import *
+#Path k castem
+pathToParts = r"C:\Users\marti\Desktop\PythonProjects\AxieInfinity\NFTImgGenerator2\Parts"
 
-allBackgroundColors = list(['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'white'])
-size = 200
-layerStep = 50 #Must be devidable by width and height
-folder = '/SVGs/'
+outputFolder = r"C:\Users\marti\Desktop\PythonProjects\AxieInfinity\NFTImgGenerator2\Output"
+
+totalImages = 1000
+allImages = []
+
+def createNewImage():
+    
+    newImage = {}
+    row = []
+
+    backgroundChoice = random.choices(backgroundNames, backgroundWeights)[0]
+    newImage ["Background"] = backgroundChoice
+    bodyChoice = random.choices(bodyNames, bodyWeights)[0]
+    newImage ["Body"] = bodyChoice
+    highlightChoice = random.choices(highlightNames, highlightWeights)[0]
+    newImage ["Highlight"] = highlightChoice
+    outlineChoice = random.choices(outlineNames, outlineWeights)[0]
+    newImage ["Outline"] = outlineChoice
+    lightChoice = random.choices(lightNames, lightWeights)[0]
+    newImage ["Light"] = lightChoice
+    
+    if newImage in allImages:
+        return createNewImage()
+    else:
+        return newImage
+    
+    
+for i in range(totalImages): 
+    
+    newTraitImage = createNewImage()
+    
+    allImages.append(newTraitImage)
+
+#True pokud nejsou duplikaty
+def allImagesUnique(allImages):
+    seen = list()
+    return not any(i in seen or seen.append(i) for i in allImages)
 
 
-def createBackground(xxx):
-    layerSize = 200
-    mySVG = SVG({'width':size, 'height':size})
-    mySVG.addChildElement('rect', {'x':0, 'y':0, 'width':layerSize, 'height':layerSize, 'fill': xxx})
-    mySVG.write('background'+str(xxx.title())+'.svg')
+print("Are all images unique?", allImagesUnique(allImages))
 
-def createAllLayersSeparately():
-    layerSize = 200
-    for layer in range(int(size/layerStep)):
-        for color in allBackgroundColors:
-            currLayerSize = layerSize-(layerStep*layer)
-            layerPos = (size/2)-((currLayerSize)/2)
-            mySVG = SVG({'width':size, 'height':size})
-            mySVG.addChildElement('rect', {'x':layerPos-5, 'y':layerPos-5, 'width':currLayerSize+10, 'height':currLayerSize+10, 'fill': 'black'})
-            mySVG.addChildElement('rect', {'x':layerPos, 'y':layerPos, 'width':currLayerSize, 'height':currLayerSize, 'fill': color})
-            currFileName = str(layer)+str(color.title())+'.svg'
-            currFilePath = folder+currFileName
-            mySVG.write(currFileName)
-            print(currFileName+' created')
-            shutil.move(currFileName, currFilePath)
-            print(currFileName+' moved to '+currFilePath)
 
-if __name__ == '__main__':
-    #for color in allBackgroundColors:
-    #    createBackground(color)
-    createAllLayersSeparately()
+
+#Prida ID ke kazdemu obrazku
+i = 0
+for item in allImages:
+    item["tokenId"] = i
+    i = i + 1
+
+#Zapsat vygenerovane nft do database
+NFTNewDatabase(allImages, r'C:\Users\marti\Desktop\PythonProjects\AxieInfinity\NFTImgGenerator2\NFTData.csv')
+
+#print(allImages)
+
+#Kolik je jakych casti
+backgroundCount = {}
+for item in backgroundNames:
+    backgroundCount[item] = 0
+    
+bodyCount = {}
+for item in bodyNames:
+    bodyCount[item] = 0
+
+highlightCount = {}
+for item in highlightNames:
+    highlightCount[item] = 0
+    
+outlineCount = {}
+for item in outlineNames:
+    outlineCount[item] = 0
+    
+lightCount = {}
+for item in lightNames:
+    lightCount[item] = 0
+
+for image in allImages:
+    backgroundCount[image["Background"]] += 1
+    bodyCount[image["Body"]] += 1
+    highlightCount[image["Highlight"]] += 1
+    outlineCount[image["Outline"]] += 1
+    lightCount[image["Light"]] += 1
+    
+print("Background count:"+str(backgroundCount))
+print("Body count:"+str(bodyCount))
+print("Highlight count:"+str(highlightCount))
+print("Outline count:"+str(outlineCount))
+print("Light count:"+str(lightCount))
+
+
+if not os.path.isdir(outputFolder):
+    os.mkdir(outputFolder)
+
+outDirFiles = os.listdir(outputFolder)
+decisionOutFolder = False
+while(not decisionOutFolder):
+    if len(outDirFiles) != 0:
+        notEmptyDir = input("Warning! Output directory is not empty. Delete contents of output folder? [Y/N]")
+        if notEmptyDir == "Y" or notEmptyDir == "y":
+            decisionOutFolder = True
+            shutil.rmtree(outputFolder)
+            print("Output folder was deleted")
+        elif notEmptyDir == "N" or notEmptyDir == "n":
+            decisionOutFolder = True
+            print("Output folder was not deleted")
+        else:
+            print("Invalid option")
+    else:
+        decisionOutFolder = True
+
+if not os.path.isdir(outputFolder):
+    os.mkdir(outputFolder)
+
+for item in allImages:
+
+    im1 = Image.open(os.path.join(pathToParts, backgroundFiles[item["Background"]])).convert('RGBA')
+    im2 = Image.open(os.path.join(pathToParts, bodyFiles[item["Body"]])).convert('RGBA')
+    im3 = Image.open(os.path.join(pathToParts, highlightFiles[item["Highlight"]])).convert('RGBA')
+    im4 = Image.open(os.path.join(pathToParts, outlineFiles[item["Outline"]])).convert('RGBA')
+    im5 = Image.open(os.path.join(pathToParts, lightFiles[item["Light"]])).convert('RGBA')
+
+    #Create each composite
+    com1 = Image.alpha_composite(im1, im2)
+    com2 = Image.alpha_composite(com1, im3)
+    com3 = Image.alpha_composite(com2, im4)
+    com4 = Image.alpha_composite(com3, im5)
+
+                     
+
+    #Convert to RGB
+    rgbIm = com4.convert('RGB')
+    fileName = str(item["tokenId"]) + ".png"
+    rgbIm.save(os.path.join(outputFolder, fileName))
+    print("Image {} created with parameters: {}".format(fileName, item))
---
+++ partsManager.py
@@ -0,0 +1,60 @@
+#Pojmenovani pro kazdou moznost pro jednodussi manipulovani
+backgroundNames = ["DarkBG", "MidBG", "LightBG"]
+#Prekladac z pojmenovani do jmena souboru
+backgroundFiles = {
+    "DarkBG": "background1.png", 
+    "MidBG": "background2.png", 
+    "LightBG": "background3.png"
+    }
+#Procentualni sance pro kazdou moznost
+backgroundWeights = [70, 20, 10]
+
+#Pojmenovani pro kazdou moznost pro jednodussi manipulovani
+bodyNames = ["PinkB", "BlueB", "LightBlueB", "GreenB", "RainbowB", "WhiteB"]
+#Prekladac z pojmenovani do jmena souboru
+bodyFiles = {
+    "PinkB": "body1.png",
+    "BlueB": "body2.png",
+    "LightBlueB": "body3.png",
+    "GreenB": "body4.png",
+    "RainbowB": "body5.png",
+    "WhiteB": "body6.png"
+    }
+#Procentualni sance pro kazdou moznost
+bodyWeights = [50, 10, 10, 20, 5, 5]
+
+#Pojmenovani pro kazdou moznost pro jednodussi manipulovani
+highlightNames = ["WhiteHL", "PinkHL", "YellowHL", "RainbowHL", "PurpleHL"]
+#Prekladac z pojmenovani do jmena souboru
+highlightFiles = {
+    "WhiteHL": "highlight1.png",
+    "PinkHL": "highlight2.png",
+    "YellowHL": "highlight3.png",
+    "RainbowHL": "highlight4.png",
+    "PurpleHL": "highlight5.png"
+    }
+#Procentualni sance pro kazdou moznost
+highlightWeights = [40, 10, 20, 10, 20]
+
+#Pojmenovani pro kazdou moznost pro jednodussi manipulovani
+outlineNames = ["RedOL", "GrayOL", "BlueOL", "WhiteOL"]
+#Prekladac z pojmenovani do jmena souboru
+outlineFiles = {
+    "RedOL": "outline1.png",
+    "GrayOL": "outline2.png",
+    "BlueOL": "outline3.png",
+    "WhiteOL": "outline4.png"
+    }
+#Procentualni sance pro kazdou moznost
+outlineWeights = [40, 40, 10, 10]
+
+#Pojmenovani pro kazdou moznost pro jednodussi manipulovani
+lightNames = ["WhiteL", "YellowL", "GreenL"]
+#Prekladac z pojmenovani do jmena souboru
+lightFiles = {
+    "WhiteL": "light1.png", 
+    "YellowL": "light2.png",
+    "GreenL": "light3.png"
+    }
+#Procentualni sance pro kazdou moznost
+lightWeights = [40, 40, 20]
